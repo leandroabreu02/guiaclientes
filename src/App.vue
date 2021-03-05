0659ae368af79ef54c73e44b3983f82d3bf41bd9
leandroabreu13 <djleandro.cardoso@gmail.com>
2021-03-05 20:45:44 -0300

    aula 23 commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div id="app">
-    <div v-for="(cliente, index) in clientes" :key="cliente.id">
+
+    <div class="buttons">
+      <button class="button is-primary">Primary</button>
+      <button class="button is-link">Link</button>
+    </div>
+
+    <h3>Cadastro: </h3>
+    <small id="nomeErro" v-show="deuErro">O nome é inválido, digite novamente</small> <br>
+    <input type="text" placeholder="nome" v-model="nomeField"> <br>
+    <input type="email" placeholder="email" v-model="emailField"> <br>
+    <input type="number" placeholder="idade" v-model="idadeField"> <br>
+    <button @click="cadastrarUsuario">Cadastrar</button>
+    <hr>
+    <div v-for="(cliente, index) in orderClientes" :key="cliente.id">
       <h4>{{ index + 1 }}</h4>
-      <Cliente :cliente="cliente"/>
-      <hr>
-      <h4>Edição: </h4>
-      <input type="text" v-model="cliente.nome">
-      <input type="text" v-model="cliente.email">
+      <Cliente :cliente="cliente" @meDelete="deleterUsuario($event)"/>
     </div>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
 import Cliente from './components/Cliente';
 // import Produto from './components/Produto';
 
@@ -19,42 +29,22 @@ export default {
   name: 'App',
   data() {
     return {
-      nomeDoLeandro: "Leandro Cardoso",
-      clienteLeandro: {
-        nome: "Leandro C",
-        email: "djleandro.cardoso@gmail.com",
-        idade: 18
-      },
+      deuErro: false,
+      nomeField: "",
+      emailField: "",
+      idadeField: 0,
       clientes: [
         {
           id: 1,
-          nome: "Pessoa1",
-          email: "p1@gmail.com",
-          idade: 20
+          nome: "Leandro Cardoso",
+          email: "djleandro.cardoso@gmail.com",
+          idade: 18
         },
         {
           id: 2,
-          nome: "Pessoa2",
-          email: "p2@gmail.com",
-          idade: 30
-        },
-        {
-          id: 3,
-          nome: "Pessoa3",
-          email: "p3@gmail.com",
-          idade: 40
-        },
-        {
-          id: 4,
-          nome: "Pessoa4",
-          email: "p4@gmail.com",
-          idade: 50
-        },
-        {
-          id: 5,
-          nome: "Pessoa5",
-          email: "p5@gmail.com",
-          idade: 60
+          nome: "Leandro Abreu",
+          email: "leandroabreu@gmail.com",
+          idade: 16
         }
       ]
     }
@@ -62,10 +52,35 @@ export default {
   components: {
     Cliente,
     // Produto
+  },
+  methods: {
+    cadastrarUsuario: function() {
+      if(this.nomeField == "" || this.nomeField == " " || this.nomeField.lengh < 3) {
+        this.deuErro = true;
+      }else {
+        this.clientes.push({ nome: this.nomeField, email: this.emailField, idade: this.idadeField, id: Date.now() })
+        this.nomeField = "";
+        this.emailField = "";
+        this.idadeField = 0;
+        this.deuErro = false;
+      }
+    },
+    deleterUsuario: function($event) {
+      var id = $event.idDoCliente;
+      var novoArray = this.clientes.filter(cliente => cliente.id != id);
+      this.clientes = novoArray;
+    }
+  },
+  computed: {
+    orderClientes: function() {
+      return _.orderBy(this.clientes, ['nome', ['asc']])
+    }
   }
 }
 </script>
 
 <style>
-
+  #nomeErro {
+    color: red;
+  }
 </style>
--- src/components/Cliente.vue
+++ src/components/Cliente.vue
@@ -1,14 +1,17 @@
 <template>
-<div :class ="{'cliente': !isPremium, 'cliente-premium': isPremium}">
-  <h4>Nome: {{ cliente.nome }}</h4>
-  <hr>
-  <p>E-mail: {{ cliente.email }}</p>
-  <p v-if="showIdade === true">Idade: {{ cliente.idade }}</p>
-  <p v-else>O usuário escondeu a idade</p>
-  <!-- <input type="text" :value="nome">     -->
-  <!-- os dois pontos servem para colocar uma variável dentro de um atribulo html -->
-  <!-- data binding, ligando um dado em um campo de formulário -->
-</div>
+  <div :class ="{'cliente': !isPremium, 'cliente-premium': isPremium}">
+    <h4>Nome: {{ cliente.nome }}</h4>
+    <hr>
+    <p>E-mail: {{ cliente.email | processarEmail }}</p>
+    <p v-if="showIdade === true">Idade: {{ cliente.idade }}</p>
+    <p v-else>O usuário escondeu a idade</p>
+    <button @click="mudarCor($event)">Mudar cor</button>
+    <button @click="emitirEventoDelete">Deletar</button>
+    <h4>Id especial: {{ idEspecial }}</h4>
+    <!-- <input type="text" :value="nome">     -->
+    <!-- os dois pontos servem para colocar uma variável dentro de um atribulo html -->
+    <!-- data binding, ligando um dado em um campo de formulário -->
+  </div>
 </template>
 
 <script>
@@ -21,6 +24,30 @@ export default {
   props: {
     cliente: Object,
     showIdade: Boolean
+  },
+  methods: {
+    mudarCor: function($event) {
+      console.log($event);
+      this.isPremium = !this.isPremium;
+    },
+    emitirEventoDelete: function() {
+      console.log('Emitindo');
+      this.$emit("meDelete", {idDoCliente: this.cliente.id, curso: "Formação Node.js", emPromocao: true, componen: this});
+    },
+    testar: function() {
+      console.log("Testando para valer");
+      alert("Isso é um alert!");
+    }
+  },
+  filters: {
+    processarEmail: function(value) {
+      return "GUIADOPROGRAMADOR." + value.toUpperCase();
+    }
+  },
+  computed: {
+    idEspecial: function() {
+      return (this.cliente.email + this.cliente.nome + this.cliente.id).toUpperCase();
+    }
   }
 }
 </script>
