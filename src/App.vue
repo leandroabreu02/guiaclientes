6a621021a40c1be65795a98af5d2c3e19b050e9f
leandroabreu13 <djleandro.cardoso@gmail.com>
2021-03-05 13:25:37 -0300

    ep14 commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="(cliente, index) in clientes" :key="cliente.id">
+      <h4>{{ index + 1 }}</h4>
+      <Cliente :cliente="cliente"/>
+      <hr>
+      <h4>Edição: </h4>
+      <input type="text" v-model="cliente.nome">
+      <input type="text" v-model="cliente.email">
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cliente from './components/Cliente';
+// import Produto from './components/Produto';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      nomeDoLeandro: "Leandro Cardoso",
+      clienteLeandro: {
+        nome: "Leandro C",
+        email: "djleandro.cardoso@gmail.com",
+        idade: 18
+      },
+      clientes: [
+        {
+          id: 1,
+          nome: "Pessoa1",
+          email: "p1@gmail.com",
+          idade: 20
+        },
+        {
+          id: 2,
+          nome: "Pessoa2",
+          email: "p2@gmail.com",
+          idade: 30
+        },
+        {
+          id: 3,
+          nome: "Pessoa3",
+          email: "p3@gmail.com",
+          idade: 40
+        },
+        {
+          id: 4,
+          nome: "Pessoa4",
+          email: "p4@gmail.com",
+          idade: 50
+        },
+        {
+          id: 5,
+          nome: "Pessoa5",
+          email: "p5@gmail.com",
+          idade: 60
+        }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    Cliente,
+    // Produto
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Cliente.vue
@@ -0,0 +1,46 @@
+<template>
+<div :class ="{'cliente': !isPremium, 'cliente-premium': isPremium}">
+  <h4>Nome: {{ cliente.nome }}</h4>
+  <hr>
+  <p>E-mail: {{ cliente.email }}</p>
+  <p v-if="showIdade === true">Idade: {{ cliente.idade }}</p>
+  <p v-else>O usuário escondeu a idade</p>
+  <!-- <input type="text" :value="nome">     -->
+  <!-- os dois pontos servem para colocar uma variável dentro de um atribulo html -->
+  <!-- data binding, ligando um dado em um campo de formulário -->
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isPremium: false 
+    }
+  },
+  props: {
+    cliente: Object,
+    showIdade: Boolean
+  }
+}
+</script>
+
+<style scoped>  
+/* esse scoped significa que esse estilo só vai ser aplicado nesse componente */
+  .cliente {
+    background-color: #ECE5E3;
+    max-width: 600px;
+    height: 180px;
+    padding: 1%;
+    margin-top: 2%;
+  }
+
+  .cliente-premium {
+    background-color: black;
+    color: yellow;
+    max-width: 600px;
+    height: 180px;
+    padding: 1%;
+    margin-top: 2%;
+  }
+</style>
